e64f436cc5b58d3580e0295ef48c55d9addcbc7c
Strangerrrs <166373671+Strangerrrs@users.noreply.github.com>
2024-04-10 07:45:28 +0100

    Fixed keepsprint

--- src/main/java/keystrokesmod/module/impl/movement/KeepSprint.java
+++ src/main/java/keystrokesmod/module/impl/movement/KeepSprint.java
@@ -1,40 +1,50 @@
 package keystrokesmod.module.impl.movement;
 
 import keystrokesmod.module.*;
+import keystrokesmod.module.impl.combat.KillAura;
 import keystrokesmod.module.setting.impl.ButtonSetting;
 import keystrokesmod.module.setting.impl.DescriptionSetting;
 import keystrokesmod.module.setting.impl.SliderSetting;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.Vec3;
 
 public class KeepSprint extends Module {
-   public static DescriptionSetting a;
-   public static SliderSetting b;
-   public static ButtonSetting c;
+   private DescriptionSetting description;
+   public static SliderSetting slow;
+   public static ButtonSetting disableWhileJump;
+   public static ButtonSetting reduceReachHits;
 
    public KeepSprint() {
       super("KeepSprint", Module.category.movement, 0);
-      this.registerSetting(a = new DescriptionSetting(new String("Default is 40% motion reduction.")));
-      this.registerSetting(b = new SliderSetting("Slow %", 40.0D, 0.0D, 40.0D, 1.0D));
-      this.registerSetting(c = new ButtonSetting("Only reduce reach hits", false));
+      this.registerSetting(description = new DescriptionSetting(new String("Default is 40% motion reduction.")));
+      this.registerSetting(slow = new SliderSetting("Slow %", 40.0D, 0.0D, 40.0D, 1.0D));
+      this.registerSetting(disableWhileJump = new ButtonSetting("Disable while jumping", false));
+      this.registerSetting(reduceReachHits = new ButtonSetting("Only reduce reach hits", false));
    }
 
-   public static void sl(Entity en) {
-      double dist;
-      if (c.isToggled() && ModuleManager.reach.isEnabled() && !mc.thePlayer.capabilities.isCreativeMode) {
-         dist = mc.objectMouseOver.hitVec.distanceTo(mc.getRenderViewEntity().getPositionEyes(1.0F));
-         double val;
-         if (dist > 3.0D) {
-            val = (100.0D - (double)((float)b.getInput())) / 100.0D;
-         } else {
-            val = 0.6D;
+   public static void keepSprint(Entity en) {
+      boolean vanilla = false;
+      if (disableWhileJump.isToggled() && !mc.thePlayer.onGround) {
+         vanilla = true;
+      } else if (reduceReachHits.isToggled() && !mc.thePlayer.capabilities.isCreativeMode) {
+         double n = -1.0;
+         final Vec3 getPositionEyes = mc.thePlayer.getPositionEyes(1.0f);
+         if (ModuleManager.killAura != null && ModuleManager.killAura.isEnabled() && KillAura.target != null) {
+            n = getPositionEyes.distanceTo(KillAura.target.getPositionEyes(1.0f));
+         } else if (ModuleManager.reach != null && ModuleManager.reach.isEnabled()) {
+            n = getPositionEyes.distanceTo(mc.objectMouseOver.hitVec);
          }
-
-         mc.thePlayer.motionX *= val;
-         mc.thePlayer.motionZ *= val;
+         if (n != -1.0 && n <= 3.0) {
+            vanilla = true;
+         }
+      }
+      if (vanilla) {
+         mc.thePlayer.motionX *= 0.6;
+         mc.thePlayer.motionZ *= 0.6;
       } else {
-         dist = (100.0D - (double)((float)b.getInput())) / 100.0D;
-         mc.thePlayer.motionX *= dist;
-         mc.thePlayer.motionZ *= dist;
+         float n2 = (100.0f - (float) slow.getInput()) / 100.0f;
+         mc.thePlayer.motionX *= n2;
+         mc.thePlayer.motionZ *= n2;
       }
    }
 }
