d4abd8242964bed7285aaae0396868e64264ed2d
SleepingPig35 <andyzeng207@gmail.com>
2024-07-08 12:58:17 +0100

    Add files via upload

---
+++ src/main/java/keystrokesmod/module/impl/other/Panic.java
@@ -0,0 +1,30 @@
+package keystrokesmod.module.impl.other;
+
+import keystrokesmod.Raven;
+import keystrokesmod.module.Module;
+import keystrokesmod.module.setting.impl.DescriptionSetting;
+import keystrokesmod.module.ModuleManager;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Panic extends Module {
+    public Panic() {
+        super("Panic", category.other);
+        this.registerSetting(new DescriptionSetting("Disables all modules."));
+    }
+
+    @Override
+    public void onEnable() {
+        List<Module> modulesToDisable = new ArrayList<>();
+        for (Module m : Raven.getModuleManager().getModules()) {
+            if (m.isEnabled()) {
+                modulesToDisable.add(m);
+            }
+        }
+        for (Module m : modulesToDisable) {
+            m.disable();
+
+        }
+        this.disable();
+    }
+}
