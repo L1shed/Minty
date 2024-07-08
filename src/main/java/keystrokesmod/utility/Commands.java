b31bdecbeb1578a48c3de287bc7927d696604b4e
SleepingPig35 <andyzeng207@gmail.com>
2024-07-08 12:56:38 +0100

    Update Commands.java

--- src/main/java/keystrokesmod/utility/Commands.java
+++ src/main/java/keystrokesmod/utility/Commands.java
@@ -266,7 +266,18 @@ public class Commands {
                         print("&a" + module.getName() + " is now visible in HUD", 1);
                     }
                 }
-            } else if (args.get(0).equals("rename")) {
+            } else if (args.get(0).equals("panic")) {
+                List<Module> modulesToDisable = new ArrayList<>();
+                for (Module m : Raven.getModuleManager().getModules()) {
+                    if (m.isEnabled()) {
+                        modulesToDisable.add(m);
+                    }
+                }
+                for (Module m : modulesToDisable) {
+                    m.disable();
+
+                }
+            }else if (args.get(0).equals("rename")) {
                 if (!hasArgs) {
                     print(invSyn, 1);
                     return;
