a8bf3a67a67c6e9e931d8cf5d7491429f15df707
Tejas Lamba <73745640+TejasLamba2006@users.noreply.github.com>
2024-07-09 23:04:44 +0530

    Fixed people not able to join Hypixel (#59)

--- src/main/java/keystrokesmod/module/impl/other/ClientSpoofer.java
+++ src/main/java/keystrokesmod/module/impl/other/ClientSpoofer.java
@@ -3,18 +3,14 @@ package keystrokesmod.module.impl.other;
 import io.netty.buffer.Unpooled;
 import keystrokesmod.Raven;
 import keystrokesmod.event.SendPacketEvent;
-import keystrokesmod.mixins.impl.network.C17PacketCustomPayloadAccessor;
 import keystrokesmod.module.Module;
 import keystrokesmod.module.setting.impl.ButtonSetting;
 import keystrokesmod.module.setting.impl.ModeSetting;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.network.play.client.C17PacketCustomPayload;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.internal.FMLProxyPacket;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Objects;
 
 
 public class ClientSpoofer extends Module {
@@ -26,7 +22,9 @@ public class ClientSpoofer extends Module {
         super("ClientSpoofer", category.other);
         this.registerSetting(mode, cancelForgePacket);
     }
-
+    public void onDisable() {
+        currentSpoof = SpoofMode.FORGE;
+    }
     @Override
     public void onUpdate() {
         currentSpoof = SpoofMode.values()[(int) mode.getInput()];
@@ -34,7 +32,6 @@ public class ClientSpoofer extends Module {
             cancelForgePacket.disable();
         } else {
             Raven.getModuleManager().getModule("ModSpoofer").enable();
-            cancelForgePacket.disable();
         }
     }
 
@@ -62,7 +59,7 @@ public class ClientSpoofer extends Module {
     }
 
     enum SpoofMode {
-        FORGE("Forge", "FML", "MC|Brand"),
+        FORGE("Forge", "FML,Forge", "MC|Brand"),
         VANILLA("Vanilla", "vanilla", "MC|Brand"),
         LUNAR("Lunar", "lunarclient:v2.16.0-2426", "MC|Brand"),
         CHEATBREAKER("Cheatbreaker", "CB", "MC|Brand"),
@@ -82,11 +79,5 @@ public class ClientSpoofer extends Module {
         public static String @NotNull [] getNames() {
             return java.util.Arrays.stream(values()).map(spoofMode -> spoofMode.name).toArray(String[]::new);
         }
-    }
-
-    @Contract("_ -> new")
-    @SuppressWarnings("All")
-    private @NotNull PacketBuffer createPacketBuffer(final @NotNull String data) {
-        return new PacketBuffer(Unpooled.buffer()).writeString(data);
     }
 }
